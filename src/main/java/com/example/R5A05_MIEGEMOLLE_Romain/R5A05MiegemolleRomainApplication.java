7b0560abf953b92cf69ca43c22ae91bcb4d24a2c
rmieg <romain.miegemolle@etu.iut-tlse3.fr>
2025-09-29 10:58:39 +0200

    feat: projet Spring initialisé + endpoint /bonjour

---
+++ src/main/java/com/example/R5A05_MIEGEMOLLE_Romain/R5A05MiegemolleRomainApplication.java
@@ -0,0 +1,13 @@
+package com.example.R5A05_MIEGEMOLLE_Romain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R5A05MiegemolleRomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R5A05MiegemolleRomainApplication.class, args);
+	}
+
+}
